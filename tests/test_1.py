58ad43be4f68626daf1d33e05078fe0557b30c41
decvalts <declan.valters@gmail.com>
2016-12-05 17:02:45 +0000

    Added test data

--- tests/test_1.py
+++ tests/test_1.py
@@ -16,8 +16,8 @@ external_file_data = "Ryedale72hours_measured.csv"
 
 # OOP way
 topmodelSensitivity = cpl.CaesarTimeseriesPlot(data_dir, "*.dat", "q_lisflood",
-                                               time_delta=60,
-                                               time_label='days',
+                                               time_delta=5,
+                                               time_label='hours',
                                                colormap='winter')
 topmodelSensitivity.plot_ensemble_hydrograph(draw_inset=False)
 topmodelSensitivity.plot_external_data(data_dir + external_file_data)
